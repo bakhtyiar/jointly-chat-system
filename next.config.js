c38e209a280b987fcab5dd2dcf5d6ba6d6eaa4e1
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-08-22 15:47:37 +0300

    added .env variables to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    HOSTNAME: process.env.HOSTNAME,
+    PORT: process.env.PORT,
+    API_HOST: process.env.API_HOST,
+    HOST_URL: process.env.HOST_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    PORT_DB: process.env.PORT_DB,
+    DB_URL: process.env.DB_URL,
+    PAGE_LIMIT: process.env.PAGE_LIMIT,
+  }
 }
 
 module.exports = nextConfig
