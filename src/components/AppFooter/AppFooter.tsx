b1797bd54632725e05c9f524e7ad963ad03bdb39
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-08-23 18:00:12 +0300

    cleaned footer

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import {pt6} from "@/utils/styleVariables";
-import styled from "@emotion/styled";
-
-const StyledFooter = styled.footer`
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  min-height: ${pt6};
-  flex: 0 0 auto;
-`
 
 const AppFooter = () => {
     return (
-        <StyledFooter>
-            <p>© 2023 No Rights Reserved. </p>
-        </StyledFooter>
+        <></>
     );
 };
 
