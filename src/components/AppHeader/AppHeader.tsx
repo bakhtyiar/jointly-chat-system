09e9dc0df97991193e366b1b231d432e93e16e9b
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-08-23 18:03:16 +0300

    styled and layouted app page

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,49 +1,75 @@
-import React from 'react';
-import {Button} from "@mui/material";
+import React, {useEffect, useState} from 'react';
+import {Avatar, Box, Button, IconButton, useTheme} from "@mui/material";
 import Link from "next/link";
 import {routes} from "@/utils/routes";
 import styled from "@emotion/styled";
-import {pt2, pt6} from "@/utils/styleVariables";
-
+import {borderRadius, pt2, pt3, pt4, pt6} from "@/utils/styleVariables";
+import {getAllCommunities, ICommunity} from "@/services/communities";
+import {CommunityAppButton} from "@/components/CommunityAppButton/CommunityAppButton";
+import AddIcon from '@mui/icons-material/Add';
+import ExploreIcon from '@mui/icons-material/Explore';
+//todo: unmock buttons, set links etc.
 const StyledHeader = styled.header`
-  width: 100%;
+  height: 100%;
   display: flex;
+  flex-grow: 1;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  min-height: ${pt6};
 `
-const StyledBrandName = styled.span`
+
+const StyledBrandLink = styled(Link)`
+  text-decoration: none;
   font-weight: 700;
   color: black;
+  transition: background-color 0.2s;
 `
 
-const StyledLinks = styled.ul`
-  list-style: none;
+const StyledCommunities = styled.div`
+  justify-self: stretch;
   display: flex;
-  gap: ${pt6};
-`
-
-const StyledLink = styled.li`
-  color: black;
+  flex-direction: column;
+  height: 100%;
+  overflow-y: auto;
+  padding-right: ${pt3};
+  padding-left: ${pt3};
 `
 
 const StyledButtons = styled.div`
   display: flex;
+  flex-direction: column;
   gap: ${pt2};
   align-items: center;
+  justify-self: end;
+  padding-bottom: ${pt4};
+  padding-top: ${pt4};
 `
 
 const AppHeader = () => {
+    let [communities, setCommunities] = useState<ICommunity[]>([]);
+
+    let fetchData = async () => {
+        let {data} = await getAllCommunities();
+        if (data) {
+            setCommunities(data)
+        }
+    }
+    //todo : check is it can be w/out hooks just next.js props
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+
     return (
         <StyledHeader>
-            <Link href={routes.index.path}><StyledBrandName>Jointly</StyledBrandName></Link>
-            <StyledLinks>
-                <StyledLink><Link href={routes.communities.path}>Communities</Link></StyledLink>
-                <StyledLink><Link href={routes.pricing.path}>Pricing</Link></StyledLink>
-            </StyledLinks>
+            <StyledBrandLink href={routes.index.path}><IconButton><Avatar>J</Avatar></IconButton></StyledBrandLink>
+            <StyledCommunities>
+                {communities && communities.map((community, index) => <CommunityAppButton
+                    key={community.name + '_' + index} avatar={community.avatar} name={community.name} id={community.id}/>)}
+            </StyledCommunities>
             <StyledButtons>
-                <Link href={routes.login.path}><Button variant="contained">Login</Button></Link>
-                <Link href={routes.register.path}><Button variant="outlined">Register</Button></Link>
+                <Link href=""><IconButton color="default"><AddIcon/></IconButton></Link>
+                <Link href=""><IconButton color="primary"><ExploreIcon/></IconButton></Link>
             </StyledButtons>
         </StyledHeader>
     );
--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import AppHeader from "@/components/AppHeader/AppHeader";
 import AppFooter from "@/components/AppFooter/AppFooter";
+import styled from "@emotion/styled";
+import {Grid} from "@mui/material";
 
-const AppLayout = ({ children }) => {
+const StyledLayout = styled(Grid)`
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+  margin: 0;
+  height: 100vh;
+  overflow-y: hidden;
+`
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const AppLayout: React.FC<Props> = ({ children }) => {
     return (
-        <>
-            <AppHeader></AppHeader>
-            {children}
-            <AppFooter></AppFooter>
-        </>
+        <StyledLayout container spacing={2}>
+            <Grid item sx={{height: '100vh'}}><AppHeader/></Grid>
+            <Grid item sx={{height: '90vh'}} xs={10}>{children}</Grid>
+        </StyledLayout>
     );
 };
 
