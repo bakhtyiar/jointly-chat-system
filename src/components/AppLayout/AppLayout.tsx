6a0d83b4e4bc130336b5587abd3968f871b35670
Bakhtiyar <bakhtiyarkhuramshin@gmail.com>
2023-08-28 13:21:33 +0300

    Code cleanup and style fixes

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,27 +1,24 @@
-import React, {ReactNode} from 'react';
+import React from 'react';
 import AppHeader from "@/components/AppHeader/AppHeader";
-import AppFooter from "@/components/AppFooter/AppFooter";
 import styled from "@emotion/styled";
-import {Grid} from "@mui/material";
 
-const StyledLayout = styled(Grid)`
+const StyledLayout = styled.div`
   display: flex;
   flex-direction: row;
   align-items: stretch;
   margin: 0;
   height: 100vh;
-  overflow-y: hidden;
+  overflow: hidden;
 `
 
 interface Props {
     children: React.ReactNode;
 }
 
-const AppLayout: React.FC<Props> = ({ children }) => {
-    return (
-        <StyledLayout container spacing={2}>
-            <Grid item sx={{height: '100vh'}}><AppHeader/></Grid>
-            <Grid item sx={{height: '90vh'}} xs={10}>{children}</Grid>
+const AppLayout: React.FC<Props> = ({children}) => {
+    return (<StyledLayout>
+            <AppHeader/>
+            {children}
         </StyledLayout>
     );
 };
