68f0dde30ebc11bf1b87e8c66f7e688de09b686a
Bakhtiyar <bakhtiyarkhuramshin@gmail.com>
2023-08-29 17:49:09 +0300

    Added FilesStack.tsx component

---
+++ src/components/FilesStack/FilesStack.tsx
@@ -0,0 +1,11 @@
+import {MessageDocument} from "@/components/MessageDocument/MessageDocument";
+
+export const FilesStack = ({}) => {
+    return (
+        <>
+            <MessageDocument/>
+            <MessageDocument/>
+            <MessageDocument/>
+        </>
+    );
+};
